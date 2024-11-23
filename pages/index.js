204c1269bfb23ad5a168045980864ed2a702f183
Diogo Felicio Demétrio Pereira <91641834+Diodoro42@users.noreply.github.com>
2024-11-23 15:50:05 +0000

    adicionando a pasta com o script javascript principal

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+function Home (){
+    return <h1>O homem que se contenta em ser apenas ele mesmo e, portanto, ser menos, vive numa prisão. Meus próprios olhos não são suficientes para mim, verei por meio dos olhos de outros. A realidade, mesmo vista por meio dos olhos de muitos, não é suficiente</h1>
+
+}
+
+export default Home;
