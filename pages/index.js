dc9d30a079b11b2c65ce98a8f69882e4c3b74247
Diogo Felicio Demétrio Pereira <91641834+Diodoro42@users.noreply.github.com>
2024-11-24 22:06:50 +0000

    adicionando mais uma frase

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ function Home (){
 
         <>
         <h2>O homem que se contenta em ser apenas ele mesmo e, portanto, ser menos, vive numa prisão. Meus próprios olhos não são suficientes para mim, verei por meio dos olhos de outros. A realidade, mesmo vista por meio dos olhos de muitos, não é suficiente.</h2>
+        <br></br>
+        <h3>Se você quiser fazer uma torta de maçã do zero, você deve primeiro criar o universo!</h3>
         <h3>Obrigado por estar aqui! 💖</h3>
         </>
         
