7772805d3223fa5ccfcc26c996b853aef85d7f4f
Diogo Felicio Demétrio Pereira <91641834+Diodoro42@users.noreply.github.com>
2024-11-26 01:20:28 +0000

    adicionando um gif, tentativa 2

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ function Home (){
         <h3>Se você quiser fazer uma torta de maçã do zero, você deve primeiro criar o universo!</h3>
         <h3>Obrigado por estar aqui! 💖</h3>
         <img src="https://respostauniversal42.files.wordpress.com/2015/02/pensador-profundo.jpg?w=772&h=9999"></img>
+        <img src="https://37.media.tumblr.com/f2ce68f53d87e05fe42bd23ea8c0f905/tumblr_ms3qgwjcHy1rf3wd1o1_500.gif"></img>
         </>
         
 
