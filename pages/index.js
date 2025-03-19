38bcc2fe8f2e6f73108de626729b8c8579092067
Diogo Felicio Demétrio Pereira <91641834+Diodoro42@users.noreply.github.com>
2025-03-19 22:49:36 +0000

    adicionando uma frase para fins didaticos

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ function Home() {
         o universo!
       </h3>
       <h3>Obrigado por estar aqui! 💖</h3>
+      <h3>Voltei depois de muito tempo</h3>
       <img src="https://respostauniversal42.files.wordpress.com/2015/02/pensador-profundo.jpg?w=772&h=9999"></img>
       <img src="https://37.media.tumblr.com/f2ce68f53d87e05fe42bd23ea8c0f905/tumblr_ms3qgwjcHy1rf3wd1o1_500.gif"></img>
     </>
