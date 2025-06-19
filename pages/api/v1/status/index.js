a747cb527fe1c45e8d3f966444d163d0db7c9df3
Diogo Felicio Demétrio Pereira <91641834+Diodoro42@users.noreply.github.com>
2025-06-19 23:54:05 +0000

    desafio do retorno da api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,12 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  console.log(database);
+  // console.log(database);
 
   const updatedAt = new Date().toISOString();
 
+  const versionRes = await database.query("SELECT version();"); //fazendo a requisição da versão
+  const version = versionRes.rows[0].version; //isolando a resposta
+  // console.log(versionRes);
+
+  const conectionsRes = await database.query("SHOW max_connections;");
+  const conections = conectionsRes.rows[0].max_connections; //isolando a resposta
+  // console.log(conectionsRes);
+
+  const useConectionsRes = await database.query(
+    "SELECT COUNT(*) FROM pg_stat_activity;",
+  );
+  const useConections = useConectionsRes.rows[0].count;
+  console.log(useConectionsRes);
+
   response.status(200).json({
     updated_at: updatedAt,
+    version: version,
+    conections: conections,
+    use_conections: useConections,
   });
 }
 
--- testes/integration/api/v1/status/get.test.js
+++ testes/integration/api/v1/status/get.test.js
@@ -5,11 +5,17 @@ test("Get to api/v1/status should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  console.log(responseBody.updated_at);
+  console.log(responseBody.version);
+  console.log(responseBody.conections);
+  console.log(responseBody.use_conections);
 
   expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.version).toBeDefined();
+  expect(responseBody.conections).toBeDefined();
+
   const parsedDate = new Date(responseBody.updated_at).toISOString();
-  console.log(parsedDate);
-  expect(new Date(responseBody.updated_at).toISOString()).toEqual(
-    responseBody.updated_at,
-  );
+  // console.log(parsedDate);
+
+  expect(responseBody.updated_at).toEqual(parsedDate);
 });
